2b006cb866396e515d2d1732dcbee965953d423a
Gülizar <73067651+GulizarA1@users.noreply.github.com>
2025-10-30 23:24:55 +0300

    Add files via upload

---
+++ ConsoleAppÖdev1.1/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+
+namespace SehirIlceOdevi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hashtable oluşturduk, şehirleri ve onların ilçelerini ekliyoruz
+            Hashtable sehirler = new Hashtable();
+
+            sehirler.Add("Bodrum", "Gümbet, Yalıkavak, Turgutreis, Bitez");
+            sehirler.Add("Antalya", "Konyaaltı, Lara, Kepez, Alanya");
+            sehirler.Add("Eskişehir", "Odunpazarı, Tepebaşı, Sivrihisar, Mahmudiye");
+
+            Console.WriteLine("İlçe sorgulama programına hoş geldiniz!");
+
+            while (true)
+            {
+                Console.Write("Bir şehir adı girin (çıkmak için 'q'): ");
+                string sehir = Console.ReadLine();
+
+                if (sehir.ToLower() == "q")
+                {
+                    Console.WriteLine("Programdan çıkılıyor...");
+                    break;
+                }
+
+                if (sehirler.ContainsKey(sehir))
+                {
+                    Console.WriteLine(sehir + " şehir merkezine bağlı ilçeler: " + sehirler[sehir]);
+                }
+                else
+                {
+                    Console.WriteLine("Bu şehir listede yok. Lütfen başka bir şehir deneyin.");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
